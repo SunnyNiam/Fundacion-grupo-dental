52983b813202bf56fb8a2a38f5f01cd9216b8288
SunnyNiam <136203042+SunnyNiam@users.noreply.github.com>
2025-07-18 12:46:58 -0500

    Delete Fundacion-grupo-dental-main_ directory

--- Fundacion-grupo-dental-main_/main.java
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mainNav = document.getElementById('mainNav');
-    const searchForm = document.getElementById('searchForm');
-
-    mainNav.addEventListener('click', function () {
-        searchForm.classList.toggle('hidden');
-    });
-});
